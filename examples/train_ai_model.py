cb64a6a02f3bed763358a7885eb6ddfda5b31cf0
Fabeon AI <eduardermakov769@gmail.com>
2025-06-11 20:24:14 +0800

    Update train_ai_model.py

--- examples/train_ai_model.py
+++ examples/train_ai_model.py
@@ -3,7 +3,7 @@
 
 import os
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
