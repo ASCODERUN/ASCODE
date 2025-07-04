8d728c26542d31001c99d7cf372db8bd88aaf1d8
Juvion AI <eduardermakov769@gmail.com>
2025-07-04 17:16:15 +0800

    Update server.py

--- ai/api/server.py
+++ ai/api/server.py
@@ -11,7 +11,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 import torch
-import torch.nn as nn
+import torch.nn as nn 
 from fastapi.responses import JSONResponse
 
 # Configure logging
