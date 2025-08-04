1ecb0cff66e656902147acb8f99b60138c2c60fc
Halnet Protocol <eduardermakov769@gmail.com>
2025-08-04 09:36:46 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os         Mubbit
+import os         Halnet
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
