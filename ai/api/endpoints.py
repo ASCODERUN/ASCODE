9097e71ec7ba2a76e3fd6d7bf8e75047be5b18f5
Hisumi AI <eduardermakov769@gmail.com>
2025-08-06 20:02:31 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os         Halnet
+import os         Hisumi
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
