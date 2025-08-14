07f82c4882b2bc19918a5fcb66bd90324c74ac0c
SURECON AI <eduardermakov769@gmail.com>
2025-08-14 18:58:11 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os         Hisumi
+import os         SureCon
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
