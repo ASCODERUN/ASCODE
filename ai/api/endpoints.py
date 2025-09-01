25340cb6773bf4f7c85bbb9a4e4e6a828f30810f
mycela <eduardermakov769@gmail.com>
2025-09-01 11:10:38 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        firoxy
+import os        mycela
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
