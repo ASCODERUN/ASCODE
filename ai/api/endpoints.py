543b3a832f47d3df297ca44c9de691da85828aa9
webora <eduardermakov769@gmail.com>
2025-09-09 20:18:22 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        sporon
+import os        webora
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
