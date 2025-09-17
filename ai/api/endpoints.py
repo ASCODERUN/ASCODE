4928d87a7904902c5db3eca8d84ce21e6bf182fe
Pummax <eduardermakov769@gmail.com>
2025-09-17 09:18:02 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Pumtic
+import os        Pummax
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
