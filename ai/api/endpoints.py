06f7595773cbce36d728fea3650fdae6a6257e59
Hooton <eduardermakov769@gmail.com>
2025-09-21 05:46:46 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        CAMFUN
+import os        Hooton
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
