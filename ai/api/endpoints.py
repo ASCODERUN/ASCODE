bb93ba5643abeea6d998b32118237f1c8b4cb124
Catoee <eduardermakov769@gmail.com>
2025-09-23 06:09:56 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        VIS1ON
+import os        Catoee
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
