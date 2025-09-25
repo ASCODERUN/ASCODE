4513f81f11ba63e7d9b72ffd2b1d7ef4c30c9d83
Frozun <eduardermakov769@gmail.com>
2025-09-26 03:08:26 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Genorx
+import os        Frozun
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
