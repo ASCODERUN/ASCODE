3359f17ce15589d997ea07f43aade09a9545c302
Dicron <eduardermakov769@gmail.com>
2025-09-27 07:34:37 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Frozun
+import os        Dicron
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
