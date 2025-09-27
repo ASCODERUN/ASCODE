4b26230396b3cb20f6d43fd6dc4319a747617ce9
ESCHIN <eduardermakov769@gmail.com>
2025-09-28 02:09:35 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Dicron
+import os        ESCHIN
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
