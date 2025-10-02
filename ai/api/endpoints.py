42db8e2067802e5ddc7714e6f830b73755596c25
FallWld <eduardermakov769@gmail.com>
2025-10-03 00:33:01 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Hootoo
+import os        FallWld
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
