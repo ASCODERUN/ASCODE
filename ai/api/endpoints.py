39a86f1eda377fbb23370e6a128b929b64f181d8
Eonium <eduardermakov769@gmail.com>
2025-10-03 05:24:43 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        FallWld
+import os        Eonium
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
