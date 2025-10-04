e080a28a46cf78b0863af280b66392ff8da0740c
Rafoon <eduardermakov769@gmail.com>
2025-10-05 02:16:20 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Ravmir
+import os        Rafoon
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
