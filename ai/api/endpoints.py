afee4902db3cd78dc73609c48600d7e266f30daa
Neruim <eduardermakov769@gmail.com>
2025-10-15 02:58:19 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        ASCODE
+import os        Neruim
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
