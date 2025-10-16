c33025be23003d936360db326aee99ba0892390e
mycoid <eduardermakov769@gmail.com>
2025-10-17 00:41:10 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Tamaon
+import os        mycoid
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
