42bfd4a10b2345581d2ede621dd4e948b04a3707
aeryon <eduardermakov769@gmail.com>
2025-10-19 02:23:36 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        HOPFUL
+import os        aeryon
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
