f6e6cdd6d7e5a50181611feb46f5fe5551f96096
Auremi <eduardermakov769@gmail.com>
2025-10-21 22:33:13 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        INZERO
+import os        Auremi
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
