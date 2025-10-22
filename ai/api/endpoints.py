db7c84a22932695f5c07beb00dd50fba8e61309c
Hootum <eduardermakov769@gmail.com>
2025-10-22 21:27:09 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        auruid
+import os        Hootum
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
