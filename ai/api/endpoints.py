39abf637692cc18f8cb71db7da0fec7095cf904b
ARCAIDX <eduardermakov769@gmail.com>
2025-10-23 20:31:53 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Hootum
+import os        ARCAIDX
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
