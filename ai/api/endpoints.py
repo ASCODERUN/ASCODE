31938b2adda8c09f5e9f0490bc7fb275d7b14514
ARCNIX <eduardermakov769@gmail.com>
2025-11-02 06:00:49 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        Owlion
+import os        ARCNIX
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
